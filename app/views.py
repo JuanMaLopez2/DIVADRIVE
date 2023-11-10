e115d6c033b49c170036f677346c230bc1e8d457
Onofre <oabenjumev@gmail.com>
2023-11-09 20:40:44 -0500

    Required to be loged in

--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def home (request):
-    #return HttpResponse('<h1>Welcome to DivaDrive</h1>')
     return render(request, 'home.html')
 
 def welcome (request):
--- divadrive/settings.py
+++ divadrive/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'   
 
 USE_I18N = True
 
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, "static"),
     'divadrive/static/',
 ]
 
